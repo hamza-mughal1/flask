2d18c169acc66c9bf69aa22f95e37d8a2dea30ab
hamza_mughal <96692951+hamza-mughal1@users.noreply.github.com>
2023-12-14 08:51:59 +0500

    Update second_website.py

--- second website/second_website.py
+++ second website/second_website.py
@@ -13,6 +13,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:hamza-100@localhost/blog_d
 
 
 Base = declarative_base()
+limit = 5
+page_no = 1
+previous_page = False
+next_page_condition = True
 
 class Blogs(Base):
 
@@ -24,15 +28,54 @@ class Blogs(Base):
     content = Column(String(120), unique=False)
     upload_date = Column(String(120), unique=False)
 
+def blog_limit(page_no,limit):
+    a = (page_no-1)*limit
+    b = page_no*limit
+    return a,b
 
+@app.route("/next-page/<int:page_no>",methods=["GET"])
+def next_page(page_no):
+    page_no+=1
+    redirect("/")
+    return home(page_reset=False,page_no = page_no)
+
+
+@app.route("/previous-page/<int:page_no>",methods=["GET"])
+def previous_page_func(page_no):
+    print(page_no)
+    page_no-=1
+    redirect("/")
+    return home(page_reset=False,page_no = page_no)
 
 @app.route("/")
-def home():
+def home(page_reset=True,page_no = 1):
+    global next_page_condition
+    global previous_page
+
+    if page_reset == True:
+        page_no = 1
+    
+
+    if page_no > 1:
+        previous_page = True
+    else:
+        previous_page = False
+
+    start, stop = blog_limit(page_no,limit)
+    start2, stop2 = blog_limit(page_no+1,limit)
     engine = sqlalchemy.create_engine("mysql+mysqlconnector://root:hamza-100@localhost/blog_db")
     Session = sessionmaker(bind=engine)
     session = Session()
-    blogs = session.query(Blogs).all()
-    return render_template("index.html",blogs=blogs)
+    blogs = session.query(Blogs).order_by(Blogs.upload_date.desc()).all()
+    
+    if len(blogs[start2:stop2]) == 0:
+        next_page_condition = False   
+    else:
+        next_page_condition = True
+
+    blogs = blogs[start:stop] 
+
+    return render_template("index.html",page_en = page_reset,blogs=blogs,previous_page=previous_page,next_page=next_page_condition,page_no=page_no)
 
 
 @app.route("/blog/<string:id>",methods=["GET"])
@@ -41,13 +84,13 @@ def get_blog(id,search_by=None):
         Session = sessionmaker(bind=engine)
         session = Session()
         if search_by == None:
-            blogs = session.query(Blogs).filter(Blogs.id == int(id)).all()
+            blogs = session.query(Blogs).filter(Blogs.id == int(id)).order_by(Blogs.upload_date.desc()).all()
         elif search_by == "author_name":
-            blogs = session.query(Blogs).filter(Blogs.author_name == id).all()
+            blogs = session.query(Blogs).filter(Blogs.author_name == id).order_by(Blogs.upload_date.desc()).all()
         elif search_by == "date":
-            blogs = session.query(Blogs).filter(Blogs.upload_date == id).all()
+            blogs = session.query(Blogs).filter(Blogs.upload_date == id).order_by(Blogs.upload_date.desc()).all()
         elif search_by == "title":
-            blogs = session.query(Blogs).filter(Blogs.title == id).all()
+            blogs = session.query(Blogs).filter(Blogs.title == id).order_by(Blogs.upload_date.desc()).all()
         return render_template("content_page.html",blogs=blogs)
 
     
@@ -94,8 +137,8 @@ def post_blog():
 
 
 
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
